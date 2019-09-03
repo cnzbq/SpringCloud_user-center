2239d68279ca57920ca60bf01a2b85d300346983
zbq <zbqdwq@gmail.com>
2019-09-03 23:14:56 +0800

    集成通用mapper

---
+++ doc/user_center.sql
@@ -0,0 +1,51 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : localhost:3306
+ Source Schema         : user_center
+
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
+
+ Date: 03/09/2019 23:13:26
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bonus_event_log
+-- ----------------------------
+DROP TABLE IF EXISTS `bonus_event_log`;
+CREATE TABLE `bonus_event_log`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id',
+  `user_id` int(11) DEFAULT NULL COMMENT 'user.id',
+  `value` int(11) DEFAULT NULL COMMENT '积分操作值',
+  `event` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '发生的事件',
+  `create_time` datetime(0) DEFAULT NULL COMMENT '创建时间',
+  `description` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `fk_bonus_event_log_user1_idx`(`user_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '积分变更记录表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `wx_id` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信id',
+  `wx_nickname` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信昵称',
+  `roles` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '角色',
+  `avatar_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '头像地址',
+  `create_time` datetime(0) NOT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NOT NULL COMMENT '修改时间',
+  `bonus` int(11) NOT NULL DEFAULT 300 COMMENT '积分',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '分享' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ src/main/java/cn/zbq/springcloud/usercenter/dao/user/UserMapper.java
@@ -0,0 +1,7 @@
+package cn.zbq.springcloud.usercenter.dao.user;
+
+import cn.zbq.springcloud.usercenter.domain.entity.user.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
