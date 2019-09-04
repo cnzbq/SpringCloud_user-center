09512a80d6f9bd9f1029f093e90ae70469018c69
zbq <zbqdwq@gmail.com>
2019-09-04 22:57:17 +0800

    测试controller

---
+++ src/main/java/cn/zbq/springcloud/usercenter/TestController.java
@@ -0,0 +1,34 @@
+package cn.zbq.springcloud.usercenter;
+
+import cn.zbq.springcloud.usercenter.dao.user.UserMapper;
+import cn.zbq.springcloud.usercenter.domain.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * TODO
+ *
+ * @author Dingwq
+ * @since 2019/9/4 22:38
+ */
+@RestController
+public class TestController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/test")
+    public User testInsert(){
+        User user = new User();
+        user.setAvatarUrl("xxx");
+        user.setBonus(100);
+        user.setCreateTime(new Date());
+        user.setUpdateTime(new Date());
+        // insert()  insert into (所有字段) values ()
+        // insertSelective  insert into (不为null的字段) values ()
+        this.userMapper.insertSelective(user);
+        return user;
+    }
+}
